e93840f04d4ab012dd053213f1c8ce8058a29b9a
Ralf Popescu <ralfpopescu@gmail.com>
2017-09-22 09:30:08 -0400

    implemented speech but have to push to ionic view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -13,6 +14,9 @@ import { EditWordsPage } from '../pages/edit-words/edit-words';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {DeactivationPage} from "../pages/deactivation/deactivation";
+
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
+import { Observable } from 'rxjs/Observable';
 
 @NgModule({
   declarations: [
@@ -43,6 +47,7 @@ import {DeactivationPage} from "../pages/deactivation/deactivation";
   providers: [
     StatusBar,
     SplashScreen,
+    SpeechRecognition,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/activation/activation.ts
+++ src/pages/activation/activation.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 import {DeactivationPage} from "../deactivation/deactivation";
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
+import { ChangeDetectorRef } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-activation',
@@ -8,9 +11,44 @@ import {DeactivationPage} from "../deactivation/deactivation";
 })
 export class ActivationPage {
 
-  constructor(public navCtrl: NavController) {
+  matches: String[];
+  isRecording = false;
 
+  constructor(public navCtrl: NavController, private speechRecognition: SpeechRecognition,
+              private plt: Platform, private cd: ChangeDetectorRef) {
+    this.startListening();
   }
+
+  isIos() {
+    return this.plt.is('ios');
+  }
+
+  stopListening() {
+    this.speechRecognition.stopListening().then(() => {
+      this.isRecording = false;
+    });
+  }
+
+  getPermission() {
+    this.speechRecognition.hasPermission()
+      .then((hasPermission: boolean) => {
+        if (!hasPermission) {
+          this.speechRecognition.requestPermission();
+        }
+      });
+  }
+
+  startListening() {
+    let options = {
+      language: 'en-US'
+    }
+    this.speechRecognition.startListening().subscribe(matches => {
+      this.matches = matches;
+      this.cd.detectChanges();
+    });
+    this.isRecording = true;
+  }
+
   goToDeactivationPage() {
     this.navCtrl.push(DeactivationPage);
   }
