9c911096883c76761de75b9447d9496b804af2c8
Janki Patel <jpatel315@gatech.edu>
2017-09-25 16:56:54 -0400

    Combined activation and deactivation onto one page

--- src/pages/activation/activation.ts
+++ src/pages/activation/activation.ts
@@ -8,20 +8,13 @@ import { NavController } from 'ionic-angular';
 export class ActivationPage {
 
   constructor(public navCtrl: NavController) {
-    document.getElementById('insideButton').addEventListener('click', function listening() {
-      if (document.getElementById('activationButton').classList.contains('deactivated')) {
-        document.getElementById('insideButton').innerText = 'Listening';
-        document.getElementById('insideButton').classList.remove('inactiveButton');
-        document.getElementById('insideButton').classList.add('listeningButton');
-        document.getElementById('activationButton').classList.remove('deactivated');
-        document.getElementById('activationButton').classList.add('activated');
-      } else {
-        document.getElementById('insideButton').innerText = 'Inactive';
-        document.getElementById('insideButton').classList.remove('listeningButton');
-        document.getElementById('insideButton').classList.add('inactiveButton');
-        document.getElementById('activationButton').classList.remove('activated');
-        document.getElementById('activationButton').classList.add('deactivated');
-      }
-    })
+
   }
+
+  active = false;
+
+  listening() {
+    this.active = !this.active;
+  }
+
 }
